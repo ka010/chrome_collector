aa720808da058f3d218926413ba9b20933fccfb7
Sven Pfleiderer <sg+github@roothausen.de>
2010-03-06 04:13:16 +0100

    first commit

---
+++ backgroundlib.js
@@ -0,0 +1,61 @@
+// Let's hide it all inside the database namespace
+
+function getDB() {
+   var db = new DataBase();
+   return db;
+}
+
+function DataBase() {
+   var logging = true;
+
+   // this is private
+   this.log = function(txt) {
+      if(logging) {
+         console.log(txt);
+      }
+   }
+}
+
+DataBase.prototype.setItem = function(key, value) {
+   try {
+      this.log("setItem: " + key + ": " + value);
+      window.localStorage.removeItem(key);
+      window.localStorage.setItem(key, value);
+   } catch(e) {
+      this.log("Error inside setItem");
+      this.log(e);
+   }
+}
+
+DataBase.prototype.getItem = function(key) {
+   var value;
+   this.log('Get Item:' + key);
+   
+   try {
+      value = window.localStorage.getItem(key);
+   } catch(e) {
+      this.log("Error inside getItem() for key:" + key);
+      this.log(e);
+      value = "null";
+   }
+   this.log("return: " + value);
+   return value;
+}
+
+DataBase.prototype.removeItem = function(key) {
+   this.log('Removing Item:' + key);
+   
+   try {
+      value = window.localStorage.removeItem(key);
+   } catch(e) {
+      this.log("Error inside removeItem() for key:" + key);
+      this.log(e);
+   }
+}
+
+DataBase.prototype.clear = function() {
+   window.localStorage.clear();
+   this.log('cleared');
+}
+
+
---
+++ popuplib.js
@@ -0,0 +1,21 @@
+
+function DB() {
+  this.db = chrome.extension.getBackgroundPage().getDB(); 
+}
+
+DB.prototype.getItem = function(key) {
+  return this.db.getItem(key); 
+}
+
+DB.prototype.setItem= function(key, value) {
+  this.db.setItem(key, value); 
+}
+
+
+DB.prototype.removeItem = function(key) {
+  this.db.removeItem(key); 
+}
+
+DB.prototype.clear = function(key) {
+  this.db.clear(); 
+}
