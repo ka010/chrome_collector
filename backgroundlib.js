13c41cd8d83e94ccdddbeeca54ad9625107039fd
Sven Pfleiderer <sg+github@roothausen.de>
2010-03-06 17:36:24 +0100

    Added usage storage to backgroundlib.js

--- backgroundlib.js
+++ backgroundlib.js
@@ -2,86 +2,145 @@
  * DB access layer with logging features for better debugging
  */
 
-var db = new DataBase();
+
+var logging = true;
 
 function getDB() {
+   var db = new DataBase();
    return db;
 }
 
-function DataBase() {
-   var logging = true;
-
-   // this is private
-   this.log = function(txt) {
-      if(logging) {
-         console.log(txt);
-      }
+log = function(txt) {
+   if(logging) {
+      console.log(txt);
    }
 }
 
+function DataBase() {
+   }
+
 DataBase.prototype.setItem = function(key, value) {
    try {
-      this.log("setItem: " + key + ": " + value);
-      window.localStorage.removeItem(key);
+      log("setItem(): " + key + ": " + value);
       window.localStorage.setItem(key, value);
    } catch(e) {
-      this.log("Error inside setItem");
-      this.log(e);
+      log("Error inside setItem");
+      log(e);
    }
 }
 
 DataBase.prototype.getItem = function(key) {
    var value;
-   this.log('Get Item:' + key);
+   log('getItem(): ' + key);
    
    try {
       value = window.localStorage.getItem(key);
    } catch(e) {
-      this.log("Error inside getItem() for key:" + key);
-      this.log(e);
+      log("Error inside getItem() for key:" + key);
+      log(e);
       value = "null";
    }
-   this.log("return: " + value);
+   log("return: " + value);
    return value;
 }
 
 DataBase.prototype.removeItem = function(key) {
-   this.log('Removing Item:' + key);
+   log('removeItem(): ' + key);
    
    try {
       window.localStorage.removeItem(key);
    } catch(e) {
-      this.log("Error inside removeItem() for key:" + key);
-      this.log(e);
+      log("Error inside removeItem() for key:" + key);
+      log(e);
    }
 }
 
+DataBase.prototype.isItemInDB = function(key) {
+   log('isItemInDB(): ' + key);
+   
+   try {
+      var item = window.localStorage.getItem(key);
+      return (item != null && item != undefined);
+   } catch(e) {
+      log("Error inside removeItem() for key:" + key);
+      log(e);
+   }
+}
+
+
 DataBase.prototype.clear = function() {
    window.localStorage.clear();
-   this.log('cleared');
+   log('cleared');
 }
 
 /*
  * Listeners to collect data and store it in HTML5 storage 
  */
 
-chrome.tabs.onCreated.addListener(function(tab) {
 
+var db = new DataBase();
+
+function getTabData(tab) {
+   var data = null;
+
+   if (db.isItemInDB(tab.url)) {
+      data = db.getItem(tab.url);
+   } else {
+      var currentDate = new Date();
+      data = {
+         "tabID": tab.id,
+         "url": tab.url,
+         "dateCreated":  currentDate, 
+         "dateLastViewed": currentDate,
+         "dateLastUpdated": currentDate,
+         "dateLastMoved": currentDate,
+         "viewCount": 0
+      };  
+   }
+
+   return data;
+}
+
+chrome.tabs.onCreated.addListener(function(tab) {
+   log("tab created: " + tab.id + ":" + tab.url);
+   db.setItem(tab.id, getTabData(tab));
 }); 
 
 
 chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
-
+   chrome.tabs.get(tabId, 
+      function(tab) {
+         log("tab selected: " + tab.id + ":" + tab.url);
+         var item = getTabData(tab);
+         item.url = tab.url;
+         item.dateLastViewed = new Date();
+         item.viewCount += 1;
+         db.setItem(tabId, item);
+      }
+   );
 }); 
 
 chrome.tabs.onMoved.addListener(function(tabId, moveInfo) {
-      
+   chrome.tabs.get(tabId, 
+      function(tab) {
+         log("tab moved: " + tab.id + ":" + tab.url);
+         var item = getTabData(tab);
+         item.url = tab.url;
+         item.dateLastMoved = new Date();
+         db.setItem(tabId, item);
+      }
+   );
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId) {
-
+   log("tab removed: " + tabId);
+   db.removeItem(tabId);
 }); 
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-}); 
+   log("tab updated: " + tab.id + ":" + tab.url);
+   var item = getTabData(tab);
+   item.url = tab.url;
+   item.dateLastUpdated = new Date();
+   db.setItem(tabId, item);
+});
