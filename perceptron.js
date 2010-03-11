7727c1aa4786e211ddaa5674639c361d784fe993
ka010 <ka010@blahdesk-2.lan>
2010-03-11 23:02:34 +0100

    implemented simple Single-Layer-Perceptron

--- perceptron.js
+++ perceptron.js
@@ -1,9 +1,63 @@
-function Perceptron(inputs) {
-    this.inputVector = Vector.Random(inputs) ;
-    this.weightsVector = Vector.Random(inputs);
-    
+function Perceptron() {
+    this.inputVector;
+    this.weightsVector;
+    this.learningRate=0.02;
 }
 
-Perceptron.prototype.calculate() {
+Perceptron.prototype.init = function(inputs) {
+    this.inputVector = Vector.Random(inputs) ;
     
+    this.weightsVector = Vector.Zero(inputs);
+    var w = this.weightsVector;
+    this.weightsVector.each(function(x,i) {
+       x =  (Math.random()) - 0.5;
+       w.elements[i-1] = x;
+    });
+}
+
+Perceptron.prototype.setInput = function(input) {
+    this.inputVector = input;
+}
+
+Perceptron.prototype.calculate = function() {
+    var current=0;
+    var sum=0.0;
+    
+    var w = this.weightsVector;
+    
+    // calculate sum
+    this.inputVector.each(function(x,i) {
+        sum = sum + x* w.e(i);
+    });
+    
+    // activation
+    if (sum >= 0) {
+        sum =1;
+    } else {
+        sum =0;
+    }
+    
+    // return class - 0/1
+    return sum;
+}
+
+Perceptron.prototype.adjustWeights = function(learningRate, output, target) {
+    var w = this.weightsVector;
+    this.inputVector.each(function(x,i) {
+        w.e(i) = w.e(i) + learningRate * (target-output) * x;
+    });
+}
+
+Perceptron.prototype.train = function(input, target) {
+    this.setInput(input);
+    var output = this.calculate();
+    this.adjustWeights(this.learningRate,output,target); 
+}
+
+function test() {
+
+ perc = new Perceptron;
+ perc.init(2);
+ var x = perc.calculate();
+   
 }
