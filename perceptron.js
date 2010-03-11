49630029a205b440a763b9ab00d5f4db9f300e1f
ka010 <ka010@blahdesk-2.lan>
2010-03-11 21:28:31 +0100

    added classifier prototype

---
+++ perceptron.js
@@ -0,0 +1,9 @@
+function Perceptron(inputs) {
+    this.inputVector = Vector.Random(inputs) ;
+    this.weightsVector = Vector.Random(inputs);
+    
+}
+
+Perceptron.prototype.calculate() {
+    
+}
