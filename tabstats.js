cf55a23f13d9c4183a43a6ee72c9725cbe4af886
Sven Pfleiderer <sg+github@roothausen.de>
2010-03-12 20:06:36 +0100

    Added scoring to tabstats

--- tabstats.js
+++ tabstats.js
@@ -7,41 +7,49 @@ function TabStats () {
    var db = new DataBase();
 
    this.addTab = function(tab) {
-      db.setItem(tab.id, getTabData(tab)); 
+      var tabData = getTabData(tab);
+      tabData.score = getScoreForTabData(tabData);
+      db.setItem(tab.id, tabData); 
    }
-   
+
    this.removeTab = function(tabId) {
+      var tabData = db.getItem(tabId);
+      addClosedTabScore(tabData.score);
       db.removeItem(tabId);
    }
 
    this.updateTabView = function(tab) {
-      var item = getTabData(tab);
-      item.dateLastViewed = new Date();
-      item.viewCount += 1;
-      db.setItem(tab.id, item);
+      var tabData = getTabData(tab);
+      tabData.dateLastViewed = new Date();
+      tabData.viewCount += 1;
+      tabData.score = getScoreForTabData(tabData);
+      db.setItem(tab.id, tabData);
    }
 
    this.updateTabMove = function(tab) {
-      var item = getTabData(tab);
-      item.dateLastMoved = new Date();
-      db.setItem(tab.id, item);
+      var tabData = getTabData(tab);
+      tabData.dateLastMoved = new Date();
+      tabData.moveCount += 1;
+      tabData.score = getScoreForTabData(tabData);
+      db.setItem(tab.id, tabData);
    }
 
    this.updateTabUpdates = function(tab) {
-      var item = getTabData(tab);
+      var tabData = getTabData(tab);
 
-      item.url = tab.url;
-      item.title = tab.title;
-      item.favIconUrl = tab.favIconUrl;
-      item.dateLastUpdated = new Date();
-      item.updateCount += 1;
-      db.setItem(tab.id, item);
+      tabData.url = tab.url;
+      tabData.title = tab.title;
+      tabData.favIconUrl = tab.favIconUrl;
+      tabData.dateLastUpdated = new Date();
+      tabData.updateCount += 1;
+      tabData.score = getScoreForTabData(tabData);
+      db.setItem(tab.id, tabData);
    }
 
    this.getAllTabs = function() {
       var all = db.getAllItems(); 
-      tabs = all.filter(function(item) {
-         return (item.tabID != undefined);
+      tabs = all.filter(function(tabData) {
+         return (tabData.tabID != undefined);
       });
       return tabs;
    }
@@ -49,6 +57,28 @@ function TabStats () {
    this.getTab = function(tabID) {
       return db.getItem(tabID);
    }
+
+   this.getAvgOpenScore = function() {
+      var allTabs = this.getAllTabs();
+      var totalScore = 0;
+      allTabs.forEach(function(tabData) {
+         totalScore += tabData.score; 
+      });
+      return (totalScore / allTabs.length);
+   }
+
+   this.getAvgClosedScore = function() {
+      var allScores = db.getItem("tabScores");
+      if (allScores != null && allScores.length > 0) {
+         var totalScore = 0;
+         allScores.forEach(function(score) {
+            totalScore += score;
+         });
+         return (totalScore / allScores.length);
+      }
+      return 0;
+   }
+
 
    function getTabData(tab) {
       if (db.isItemInDB(tab.id)) {
@@ -65,8 +95,25 @@ function TabStats () {
             "dateLastUpdated": currentDate,
             "dateLastMoved": currentDate,
             "viewCount": 0,
-            "updateCount": 0
+            "moveCount": 0,
+            "updateCount": 0,
+            "score": 0
          };  
       }
+   }
+
+   function addClosedTabScore(score) {
+      var allScores = db.getItem("tabScores");
+      if (allScores == null) {
+         allScores = new Array();
+      }
+      allScores.push(score);
+      db.setItem("tabScores", allScores);
+   }
+
+
+   function getScoreForTabData(tabData) {
+      var score = tabData.viewCount + tabData.moveCount + tabData.updateCount; 
+      return score;
    }
 }
