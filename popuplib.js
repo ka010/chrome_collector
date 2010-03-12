7a4d8f8d0f1425c536665abfeb80f77b083b104e
ka010 <ka010@blahdesk-2.lan>
2010-03-12 21:38:30 +0100

    local merge

--- popuplib.js
+++ popuplib.js
@@ -8,20 +8,22 @@ function render() {
    var button = "<div class='container'><input type='button' value='Clean Up!' onClick='cleanUp();'/></div>"
    $('body').append(button);
    
-   var tabs = tabStats.getAllTabs();
+   var tabs = tabStats.getCollectedTabs();
    for (count in tabs) {
       if (count != null) {
          var tab = tabs[count];
+         var thumb = "<img class='thumb' width='250px' height='250px' src='"+tab.thumbnail+"'/>";
          var item = "<div id ='"+tab.tabID+ "' class='container'><img src='" + tab.favIconUrl + "' alt='favicon'/><input type='checkbox' name='" + tab.url + "' value='" + tab.tabID + "'>" + tab.title + "</div>";
-
+         
          $('body').append(item);
-
+         
          
       } 
+    
    }
    
    $('.container').click(function() {
- 
+
    $("input[value="+this.id+"]").attr('checked',!($("input[value="+this.id+"]").attr('checked')));
    });
 
